ead8e188b8fee24cf36c08bc31cd2d06cdb77d8f
dah <cdh010126r@gmail.com>
2024-06-15 14:41:49 +0900

    목록, 현재금액 갱신(추가 삭제)

--- js/store.js
+++ js/store.js
@@ -4,7 +4,7 @@
     id: "2",
   }[]
  * detailList {
-    2: {
+    todayId: {  //2
        id: Date.now() + 1000,
        createAt: new Date(),
        description: "삼겹살",
@@ -49,13 +49,13 @@ export function initStore() {
 
 export function addNewHistory(newHistory) {
   try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList = null;
-    store.currentFunds = null;
+    if (store.detailList[store.todayId]) {
+      store.detailList[store.todayId].push(newHistory);
+    } else {
+      store.detailList[store.todayId] = [newHistory];
+    }
+
+    store.currentFunds -= newHistory.amount;
 
     updateStorage();
     return true;
@@ -67,12 +67,14 @@ export function addNewHistory(newHistory) {
 
 export function removeHistory(dateId, itemId) {
   try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList[dateId] = null;
+    store.detailList[dateId] = store.detailList[dateId].filter(
+      ({ id, amount }) => {
+        if (id === Number(itemId)) {
+          store.currentFunds += amount;
+        }
+        return id !== Number(itemId);
+      }
+    );
 
     updateStorage();
     return true;
